6ff8aab6c642c5360edf8f7e431c0e791b59983f
5oClutch <emailtonisarg@gmail.com>
2017-12-30 02:04:15 -0500

    Config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql100.byethost32.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'b32_18635773_tsstats'),
+            'username' => env('DB_USERNAME', 'b32_18635773'),
+            'password' => env('DB_PASSWORD', 'Basketball123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
